2dc5b23df89a78488036fa90ca2d44db3468b53f
Denis <grabko.denis11@gmail.com>
2023-12-08 14:28:55 +0200

    Update trending-service.js

--- src/api-service/trending-service.js
+++ src/api-service/trending-service.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-//axios.defaults.baseURL = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
 
-const API_KEY = '8174f1744af1a46c4588fe70a373352a';
+const KEY = '8174f1744af1a46c4588fe70a373352a';
+
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 axios.defaults.params = {
-    API_KEY,
+  key: KEY,
 };
 
 const options = {
@@ -17,43 +17,88 @@ const options = {
 };
 
 export const getTrending = async () => {
+  const { data } = await axios.get('trending/movie/day?language=en-US', options);
+  return data.results;
+};
 
-    const { data } = await axios.get(
-        'trending/movie/day?language=en-US',
-        options
-    );
-    return data.results;
-    };
- 
-export const getDetailInfoById = async (id) => {   
-    const { data } = await axios.get(
-        `/movie/${id}`,
-        options
-    );
-    return data;
-}
+export const getDetailInfoById = async (id) => {
+  const { data } = await axios.get(`/movie/${id}`, options);
+  return data;
+};
 
 export const getActorsList = async (id) => {
-    const { data } = await axios.get(
-        `movie/${id}/credits`,
-        options
-    );
-    return data.cast;
-}
+  const { data } = await axios.get(`movie/${id}/credits`, options);
+  return data.cast;
+};
 
 export const getMovieReviews = async (id) => {
-    const { data } = await axios.get(
-        `movie/${id}/reviews`,
-        options
-    );
-    return data.results;
-}
+  const { data } = await axios.get(`movie/${id}/reviews`, options);
+  return data.results;
+};
 
 export const SearchMovieByName = async (name) => {
-    const { data } = await axios.get(
-    `search/movie?query=${name}&include_adult=false&language=en-US&page=1`,
-        options
-    );
-    return data;
-}
+  const { data } = await axios.get(`search/movie?query=${name}&include_adult=false&language=en-US&page=1`, options);
+  return data;
+};
+
+
+
+// import axios from "axios";
+// //axios.defaults.baseURL = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+
+// const KEY = '8174f1744af1a46c4588fe70a373352a';
+// axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+// axios.defaults.params = {
+//     api_key: KEY,
+// };
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization:
+//       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NGJmYmIyNTA2NmM5ZTJhNjhkNzA0NmFkNTgxYjBlYyIsInN1YiI6IjY1NmI0YTY5MDg1OWI0MDEzOTUxMmZhMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JHtQht9lLzVGn8qQlSLZwC1_-cT8AoKj26qDOrlg0qc',
+//   },
+// };
+
+// export const getTrending = async () => {
+
+//     const { data } = await axios.get(
+//         'trending/movie/day?language=en-US',
+//         options
+//     );
+//     return data.results;
+//     };
+
+// export const getDetailInfoById = async (id) => {
+//     const { data } = await axios.get(
+//         `/movie/${id}`,
+//         options
+//     );
+//     return data;
+// }
+
+// export const getActorsList = async (id) => {
+//     const { data } = await axios.get(
+//         `movie/${id}/credits`,
+//         options
+//     );
+//     return data.cast;
+// }
+
+// export const getMovieReviews = async (id) => {
+//     const { data } = await axios.get(
+//         `movie/${id}/reviews`,
+//         options
+//     );
+//     return data.results;
+// }
+
+// export const SearchMovieByName = async (name) => {
+//     const { data } = await axios.get(
+//     `search/movie?query=${name}&include_adult=false&language=en-US&page=1`,
+//         options
+//     );
+//     return data;
+// }
 
