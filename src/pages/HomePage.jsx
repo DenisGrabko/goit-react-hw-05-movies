83b8603ba44541164ca38b7d725bfc432f1762f3
Denis <grabko.denis11@gmail.com>
2023-12-09 14:04:57 +0200

    anti-dubling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { getTrending } from "api-service/trending-service";
-// import { NavLink } from "react-router-dom";
-import { FlexItemList, 
-         StyledLink, 
-         FilmTitleImage, 
-         FlexItemDescription, 
-         TitleFilm, 
-         ShellForItemText, 
-         FlexItemTextStyle } from "./HomePage.styled";
+import MovieList from "components/MovieList/MovieList";
+// import { FlexItemList, 
+//          StyledLink, 
+//          FilmTitleImage, 
+//          FlexItemDescription, 
+//          TitleFilm, 
+//          ShellForItemText, 
+//          FlexItemTextStyle } from "./HomePage.styled";
 import { Outlet } from "react-router-dom";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import Notiflix from "notiflix";
@@ -25,41 +25,44 @@ const HomePage = () => {
   return (
     <>
     <section>
-      <FlexItemList>
-        {data.map(
-          ({
-            id,
-            original_title,
-            release_date,
-            poster_path,
-            vote_average,          
-          }) => ( 
-            <li key={id}>
-              <StyledLink to={`movies/${id}`}>
-                <FilmTitleImage
-                  src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
-                  alt={original_title}
-                  >                  
-                </FilmTitleImage>
-                <div>
-                  <FlexItemDescription>
-                    <TitleFilm>{original_title}</TitleFilm>
-                    <ShellForItemText>
-                      <FlexItemTextStyle>{release_date.slice(0, 4)}</FlexItemTextStyle>
-                      <FlexItemTextStyle>{vote_average === 0 ? '-' : vote_average.toFixed(1)}🏆</FlexItemTextStyle>
-                    </ShellForItemText>                    
-                  </FlexItemDescription>
-                </div>
-              </StyledLink>
-            </li>)
-        )}
-      </FlexItemList>      
+      <div>
+        <MovieList movies={data}/>
+      </div>       
     </section>
     <Outlet /> 
     </>
   )
 }
 
-HomePage.propTypes = {};
+ HomePage.propTypes = {};
 
-export default HomePage;
+ export default HomePage;
+//<FlexItemList>
+//         {data.map(
+//           ({
+//             id,
+//             original_title,
+//             release_date,
+//             poster_path,
+//             vote_average,          
+//           }) => ( 
+//             <li key={id}>
+//               <StyledLink to={`movies/${id}`}>
+//                 <FilmTitleImage
+//                   src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
+//                   alt={original_title}
+//                   >                  
+//                 </FilmTitleImage>
+//                 <div>
+//                   <FlexItemDescription>
+//                     <TitleFilm>{original_title}</TitleFilm>
+//                     <ShellForItemText>
+//                       <FlexItemTextStyle>{release_date.slice(0, 4)}</FlexItemTextStyle>
+//                       <FlexItemTextStyle>{vote_average === 0 ? '-' : vote_average.toFixed(1)}🏆</FlexItemTextStyle>
+//                     </ShellForItemText>                    
+//                   </FlexItemDescription>
+//                 </div>
+//               </StyledLink>
+//             </li>)
+//         )}
+//       </FlexItemList>   
