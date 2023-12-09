e857321043b6197ec2a5593458a156e81bdeb236
Denis <grabko.denis11@gmail.com>
2023-12-09 13:14:04 +0200

    search-setting

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,10 @@
-import React from 'react';   //{ useState, useEffect }
-// import { useLocation, useParams } from 'react-router-dom';
+import React from 'react';  //, { useState, useEffect } 
+import { useLocation,  Link } from 'react-router-dom'; //useParams,
 // import { Notify } from 'notiflix';
 // import { getDetailInfoById } from 'api-service/trending-service';
 import {
     FlexItemList,
-    StyledLink,
+    // StyledLink,
     FilmTitleImage,
     FlexItemDescription,
     TitleFilm,
@@ -14,53 +14,51 @@ import {
 
 
 const MovieList = ({ movies }) => {
-    // const [movie, setMovie] = useState(null);
-    // const { movieId } = useParams();
-    // const location = useLocation();
+    
+    const location = useLocation();
+    
+    return (      
+        <section>
+            <FlexItemList>
+                {movies.map(
+                    ({
+                        id,
+                        original_title,
+                        release_date,
+                        poster_path,
+                        vote_average,          
+                    }) => ( 
+                        <li key={id}>
+                            <Link to={`/movies/${id}`} state={{ from: location }}>
+                                <FilmTitleImage
+                                    src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
+                                    alt={original_title}
+                                />
+                                <div>
+                                    <FlexItemDescription>
+                                        <TitleFilm>{original_title}</TitleFilm>
+                                        <ShellForItemText>
+                                            <FlexItemTextStyle>{release_date.slice(0, 4)}</FlexItemTextStyle>
+                                            <FlexItemTextStyle>{vote_average === 0 ? '-' : vote_average.toFixed(1)}🏆</FlexItemTextStyle>
+                                        </ShellForItemText>                    
+                                    </FlexItemDescription>
+                                </div>
+                            </Link>
+                        </li>)
+                )}
+            </FlexItemList>      
+        </section>      
+    );
+};
 
+export default MovieList;
+
+
+//const [movie, setMovie] = useState(null);
+    // const { movieId } = useParams();
+   
     // useEffect(() => {
     //     getDetailInfoById(movieId)
-    //         .then(setMovie);
-    
-    //   return () => {
-    //       Notify.failure('Something went wrong');
-    //   }
-    // }, [movieId])
-         
-
-
-  return (      
-      <section>
-      <FlexItemList>
-        {movies.map(
-          ({
-            id,
-            original_title,
-            release_date,
-            poster_path,
-            vote_average,          
-          }) => ( 
-            <li key={id}>
-                    <StyledLink to={`movies/:${id}`}>   {/* state={{from: location} */}
-                <FilmTitleImage
-                  src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
-                  alt={original_title}
-                  >                  
-                </FilmTitleImage>
-                <div>
-                  <FlexItemDescription>
-                    <TitleFilm>{original_title}</TitleFilm>
-                    <ShellForItemText>
-                      <FlexItemTextStyle>{release_date.slice(0, 4)}</FlexItemTextStyle>
-                      <FlexItemTextStyle>{vote_average === 0 ? '-' : vote_average.toFixed(1)}🏆</FlexItemTextStyle>
-                    </ShellForItemText>                    
-                  </FlexItemDescription>
-                </div>
-              </StyledLink>
-            </li>)
-        )}
-      </FlexItemList>      
-    </section>      
-  )
-}
-export default MovieList;
+    //         .then(setMovie)
+    //         .catch(() => Notify.failure('Something went wrong'));
+    // }, [movieId]);
